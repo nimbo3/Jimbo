635c5cb5fbecefbade6e705a488e514ef3807648
ali aliabadi <al.aliabadi@mail.sbu.ac.ir>
2019-07-20 19:39:22 +0430

    add functions for saving url(robots.txt remains)

--- crawler/src/main/java/in/nimbo/jimbo/Connection/ConnectionRedis.java
+++ crawler/src/main/java/in/nimbo/jimbo/Connection/ConnectionRedis.java
@@ -1,29 +1,47 @@
 package in.nimbo.jimbo.Connection;
 
 import org.redisson.Redisson;
+import org.redisson.api.RMapCache;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * class for connecting to redis database (LRU cache)
  */
 public class ConnectionRedis {
 
-    private RedissonClient redissonClient;
+    RedissonClient redissonClient;
+    RMapCache<String, String> urls;
+    Config config;
 
     public ConnectionRedis(String hostPort1, String password1, String hostPort2) {
 
-        Config config = new Config();
+        config = new Config();
         config.useReplicatedServers()
                 .addNodeAddress("redis://" + hostPort1, "redis://" + hostPort2).setPassword(password1);
         redissonClient = Redisson.create(config);
+        urls = redissonClient.getMapCache("urls");
     }
 
-    void addToDb(String key, String value, int expiredTimeSecond) {
+    void addLinkToDb(String key, String value, int expiredTimeSecond) {
+        if (redissonClient.isShutdown()) {
+            redissonClient = Redisson.create(config);
+        }
+        // the result is for logging
+        boolean result = urls.fastPut(key, value, expiredTimeSecond, TimeUnit.SECONDS);
+
+        if (!result) {
+            //
+        }
     }
 
     boolean existsInDB(String key) {
-        return false;
+        if (redissonClient.isShutdown()) {
+            redissonClient = Redisson.create(config);
+        }
+        return urls.containsKey(key);
     }
 
 }
