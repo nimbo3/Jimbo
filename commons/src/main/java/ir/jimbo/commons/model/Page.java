8339fdf19d38bd49e50f07eadee342d33963227f
smsk <mohammadsadeghkeshavarzi@yahoo.com>
2019-07-21 11:55:42 +0430

    Modify class Page

--- commons/src/main/java/ir/jimbo/commons/model/Page.java
+++ commons/src/main/java/ir/jimbo/commons/model/Page.java
@@ -1,17 +1,14 @@
 package ir.jimbo.commons.model;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Page {
     String url;
     String title;
     Map<String, String> metadata = new HashMap<>();
     Map<String, String> links = new HashMap<>();
-    Set<String> h1 = new HashSet<>();
-    Set<String> h2 = new HashSet<>();
-    Set<String> h3to6 = new HashSet<>();
-    Set<String> pres = new HashSet<>();
+    List<String> h1 = new ArrayList<>();
+    List<String> h2 = new ArrayList<>();
+    List<String> h3to6 = new ArrayList<>();
+    List<String> plainText = new ArrayList<>(); //<p> and <pre> tags
 }
