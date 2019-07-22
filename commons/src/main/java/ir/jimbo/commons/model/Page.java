8c03081ba2b0d238c96406b7e907a44efe286454
smsk <mohammadsadeghkeshavarzi@yahoo.com>
2019-07-22 12:42:35 +0430

    Add getter for page.links

--- commons/src/main/java/ir/jimbo/commons/model/Page.java
+++ commons/src/main/java/ir/jimbo/commons/model/Page.java
@@ -1,11 +1,17 @@
 package ir.jimbo.commons.model;
 
-import java.util.*;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Page {
     String url;
     String title;
     Map<String, String> metadata = new HashMap<>();
+    @Getter
     Map<String, String> links = new HashMap<>();
     List<String> h1 = new ArrayList<>();
     List<String> h2 = new ArrayList<>();
