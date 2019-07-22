e90aaca2541da4d6968e743b6f788ff13d9c3251
ali aliabadi <al.aliabadi@mail.sbu.ac.ir>
2019-07-22 11:47:51 +0430

    improve crawl

--- crawler/src/main/java/ir/jimbo/crawler/parse/PageParser.java
+++ crawler/src/main/java/ir/jimbo/crawler/parse/PageParser.java
@@ -21,45 +21,6 @@ public class PageParser {
         Document document = Jsoup.parse(url);
         Page page = new Page();
 
-        // Adding H1
-        for (Element element : document.getElementsByTag("h1")) {
-            String text = element.text();
-            if(text == null)
-                text = "";
-            page.getH1List().add(text);
-        }
-
-        // Adding H2
-        for (Element element : document.getElementsByTag("h2")) {
-            String text = element.text();
-            if(text == null)
-                text = "";
-            page.getH2List().add(text);
-        }
-
-        // Adding links
-        for (Element element : document.getElementsByTag("a")) {
-            String href = element.attr("href");
-            if(href == null)
-                href = "";
-            String text = element.text();
-            if(text == null)
-                text = "";
-            page.getLinks().put(text, href);
-        }
-
-        // Adding meta tags
-        for (Element element : document.getElementsByTag("meta")) {
-            String name = element.attr("name");
-            if(name == null)
-                name = "";
-            String content = element.attr("content");
-            if(content == null)
-                content = "";
-            page.getMetadata().put(name, content);
-        }
-
-        // Adding plain texts. and h3 to h6
         for(Element element : document.getAllElements()) {
             Set<String> h3to6Tags = new HashSet<>(Arrays.asList("h3", "h4", "h5", "h6"));
             Set<String> plainTextTags = new HashSet<>(Arrays.asList("p", "span", "pre"));
@@ -70,6 +31,24 @@ public class PageParser {
                 page.getH3to6List().add(text);
             if(plainTextTags.contains(element.tagName()))
                 page.getPlainTextList().add(text);
+            if(element.tagName().equals("h1"))
+                page.getH1List().add(text);
+            if (element.tagName().equals("h2"))
+                page.getH2List().add(text);
+            if (element.tagName().equals("a")) {
+                String href = element.attr("href");
+                if(href == null)
+                    href = "";
+                page.getLinks().put(text, href);
+            } if (element.tagName().equals("meta")) {
+                String name = element.attr("name");
+                if(name == null)
+                    name = "";
+                String content = element.attr("content");
+                if(content == null)
+                    content = "";
+                page.getMetadata().put(name, content);
+            }
         }
 
         return page;
