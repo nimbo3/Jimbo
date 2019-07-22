b01a3ba5d10ad07a9fd84ec159f2df32829f0265
ali aliabadi <al.aliabadi@mail.sbu.ac.ir>
2019-07-22 10:01:04 +0430

    add methods for process link(still empty

--- crawler/src/main/java/ir/jimbo/crawler/ProcessLink.java
+++ crawler/src/main/java/ir/jimbo/crawler/ProcessLink.java
@@ -4,10 +4,29 @@ public class ProcessLink extends Thread{
 
     String title;
     String url;
+    RedisConnection redis;
 
-    public ProcessLink(String title, String url) {
+    public ProcessLink(String title, String url, RedisConnection redis) {
         this.title = title;
         this.url = url;
+        this.redis = redis;
     }
 
+    @Override
+    public void run() {
+        String domain = getDomain(url);
+        if (! redis.existsDomainInDB(domain)) {
+            if (checkValid(url)) {
+                redis.addDomainInDb(domain, url);
+            }
+        }
+    }
+
+    private boolean checkValid(String url) {
+        return false;
+    }
+
+    private String getDomain(String url) {
+
+    }
 }
