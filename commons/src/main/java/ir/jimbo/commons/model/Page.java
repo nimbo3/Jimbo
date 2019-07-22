9c5a252a4564a2b187cf2e19a2f583aa84784c24
smsk <mohammadsadeghkeshavarzi@yahoo.com>
2019-07-22 18:40:54 +0430

    Cmd + Alt + l in Page

--- commons/src/main/java/ir/jimbo/commons/model/Page.java
+++ commons/src/main/java/ir/jimbo/commons/model/Page.java
@@ -3,7 +3,11 @@ package ir.jimbo.commons.model;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 @Setter
