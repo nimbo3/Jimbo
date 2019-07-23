ffa74eb8386656683303c8eeea8409690e7b4e1f
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-07-23 11:23:14 +0430

    page parser code removed from page parser and add to kafka

--- crawler/src/main/java/ir/jimbo/crawler/parse/PageParseAndAddToKafka.java
+++ crawler/src/main/java/ir/jimbo/crawler/parse/PageParseAndAddToKafka.java
@@ -1,18 +1,10 @@
 package ir.jimbo.crawler.parse;
 
-import ir.jimbo.commons.model.HtmlTag;
 import ir.jimbo.commons.model.Page;
 import ir.jimbo.crawler.Parsing;
 import ir.jimbo.crawler.exceptions.NoDomainFoundException;
 import ir.jimbo.crawler.kafka.PageProducer;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -48,61 +40,10 @@ public class PageParseAndAddToKafka extends Parsing implements Runnable {
                 repeat = false;
                 e.printStackTrace();
             }
-            Page page = parse();
+            Page page = new PageParser(this.url).parse();
             producer.addPageToKafka(pagesTopicName, page);
             redis.addDomainInDb(getDomain(url));
         }
-    }
-
-    Page parse() {
-        Document document;
-        Page page = new Page();
-        try {
-            document = Jsoup.connect(url).get();
-        } catch (IOException e) {
-            // TODO: Log
-            e.printStackTrace();
-            return page;
-        }
-
-        for (Element element : document.getAllElements()) {
-            Set<String> h3to6Tags = new HashSet<>(Arrays.asList("h3", "h4", "h5", "h6"));
-            Set<String> plainTextTags = new HashSet<>(Arrays.asList("p", "span", "pre"));
-            String text = element.text();
-            if (text == null)
-                text = "";
-            if (h3to6Tags.contains(element.tagName().toLowerCase()))
-                page.getH3to6List().add(new HtmlTag(element.tagName(), element.text()));
-            else if (plainTextTags.contains(element.tagName().toLowerCase()))
-                page.getPlainTextList().add(new HtmlTag(element.tagName(), element.text()));
-            else if (element.tagName().equalsIgnoreCase("h1"))
-                page.getH1List().add(new HtmlTag("h1", element.text()));
-            else if (element.tagName().equalsIgnoreCase("h2"))
-                page.getH2List().add(new HtmlTag("h2", element.text()));
-            else if (element.tagName().equalsIgnoreCase("title"))
-                page.setTitle(element.text());
-            else if (element.tagName().equalsIgnoreCase("a")) {
-                String href = element.attr("abs:href");
-                if (href == null)
-                    href = "";
-                HtmlTag linkTag = new HtmlTag("a", element.text());
-                linkTag.getProps().put("href", href);
-                page.getLinks().add(linkTag);
-            } else if (element.tagName().equalsIgnoreCase("meta")) {
-                String name = element.attr("name");
-                if (name == null)
-                    name = "";
-                String content = element.attr("content");
-                if (content == null)
-                    content = "";
-                HtmlTag metaTag = new HtmlTag("meta");
-                metaTag.getProps().put("name", element.attr("name"));
-                metaTag.getProps().put("content", element.attr("content"));
-                page.getMetadata().add(metaTag);
-            }
-        }
-
-        return page;
     }
 
     private String getDomain(String url) throws NoDomainFoundException {
