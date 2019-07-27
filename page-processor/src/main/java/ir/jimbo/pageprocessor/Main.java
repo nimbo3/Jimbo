25fa758c8627567ce6c9853c6c443492650e0db8
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-07-27 13:00:30 +0430

    delete test main file

--- page-processor/src/main/java/ir/jimbo/pageprocessor/Main.java
+++
@@ -1,9 +0,0 @@
-package ir.jimbo.pageprocessor;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-public class Main {
-    public static void main(String[] args) throws JsonProcessingException {
-
-    }
-}
