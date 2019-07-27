861791dedb8fbe727db0b3981f0553cc64bab432
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-07-27 17:20:47 +0430

    URL Exists check added

--- crawler/src/main/java/ir/jimbo/crawler/service/CacheService.java
+++ crawler/src/main/java/ir/jimbo/crawler/service/CacheService.java
@@ -11,10 +11,9 @@ import redis.clients.jedis.Jedis;
  * class for connecting to redis database (LRU cache)
  */
 public class CacheService {
-
-
     private Logger logger = LogManager.getLogger(this.getClass());
     private int expiredTimeDomainMilis;
+    private int expiredTimeUrlMilis;
     private Jedis jedis;
 
     public CacheService(RedisConfiguration redisConfiguration) {
@@ -28,12 +27,17 @@ public class CacheService {
             }
         }));
         jedis = new Jedis();
-        expiredTimeDomainMilis = redisConfiguration.getExpiredTime();
+        expiredTimeDomainMilis = redisConfiguration.getDomainExpiredTime();
+//        expiredTimeUrlMilis =
         logger.info("redis connection created.");
     }
 
     public void addDomain(String domain) {
         jedis.set(domain, String.valueOf(System.currentTimeMillis()));
+    }
+
+    public void addUrl(String url) {
+        jedis.set(url, String.valueOf(System.currentTimeMillis()));
     }
 
     public boolean isDomainExist(String key) {
@@ -48,6 +52,22 @@ public class CacheService {
             return true;
         } else {
             jedis.del(key);
+            return false;
+        }
+    }
+
+    public boolean isUrlExists(String url) {
+        long lastTime;
+        try {
+            lastTime = Long.parseLong(jedis.get(url));
+        } catch (Exception e) {
+            return true;
+        }
+        long currentTime = System.currentTimeMillis();
+        if (currentTime - lastTime < expiredTimeUrlMilis) {
+            return true;
+        } else {
+            jedis.del(url);
             return false;
         }
     }
