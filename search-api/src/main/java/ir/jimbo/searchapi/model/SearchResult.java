8c558ba2711dcf93f0ec929742e395cb36507be9
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-07-31 20:37:39 +0430

    define searchItem class

--- search-api/src/main/java/ir/jimbo/searchapi/model/SearchResult.java
+++ search-api/src/main/java/ir/jimbo/searchapi/model/SearchResult.java
@@ -1,34 +1,12 @@
 package ir.jimbo.searchapi.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.io.Serializable;
-import java.util.Objects;
+import java.util.List;
 
-@Setter
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
 public class SearchResult implements Serializable {
-    private String title;
-    private String text;
-    private String url;
+    private List<SearchItem> searchItemList;
+    private long searchTime;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        SearchResult that = (SearchResult) o;
-        return Objects.equals(title, that.title) &&
-                Objects.equals(text, that.text) &&
-                Objects.equals(url, that.url);
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(title, text, url);
-    }
+
 }
