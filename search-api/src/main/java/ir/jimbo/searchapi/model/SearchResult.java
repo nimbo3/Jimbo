9772125f70714d5918974eb03fd932a6771cc220
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-07-31 20:38:54 +0430

    define searchResult methods

--- search-api/src/main/java/ir/jimbo/searchapi/model/SearchResult.java
+++ search-api/src/main/java/ir/jimbo/searchapi/model/SearchResult.java
@@ -1,12 +1,36 @@
 package ir.jimbo.searchapi.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SearchResult implements Serializable {
     private List<SearchItem> searchItemList;
     private long searchTime;
 
+    public SearchResult() {
+        searchItemList = new ArrayList<>();
+        searchTime = 0;
+    }
 
+    public SearchResult(List<SearchItem> searchItemList, long searchTime) {
+        this.searchItemList = searchItemList;
+        this.searchTime = searchTime;
+    }
 
+    public List<SearchItem> getSearchItemList() {
+        return searchItemList;
+    }
+
+    public void setSearchItemList(List<SearchItem> searchItemList) {
+        this.searchItemList = searchItemList;
+    }
+
+    public long getSearchTime() {
+        return searchTime;
+    }
+
+    public void setSearchTime(long searchTime) {
+        this.searchTime = searchTime;
+    }
 }
