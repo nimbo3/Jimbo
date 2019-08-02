f2c76e658d352f0d59604a501f81cd882da54ca7
seyedalireza <shosseini@ce.sharif.edu>
2019-08-02 13:30:16 +0430

    add ranking manager module

---
+++ es-page-processor/script.sh
@@ -0,0 +1,7 @@
+mvn clean package -DskipTests
+echo "sending to server4 (46.4.40.237)"
+ssh -p 3031 jimbo@46.4.40.237 'rm es-page-processor-0.0.1-SNAPSHOT.jar'
+scp -P 3031 target/es-page-processor-0.0.1-SNAPSHOT.jar jimbo@46.4.40.237:~/
+echo "sending to servers from server3"
+ssh -p 3031 jimbo@46.4.40.237 'scp -P 3031 es-page-processor-0.0.1-SNAPSHOT.jar jimbo@144.76.119.111:~/;echo "done sending to server1 (144.76.119.111)";scp -P 3031 es-page-processor-0.0.1-SNAPSHOT.jar jimbo@144.76.24.115:~/;echo "done sending to server2 (144.76.24.115)";scp -P 3031 es-page-processor-0.0.1-SNAPSHOT.jar jimbo@5.9.110.169:~/;echo "done sending to server3 (5.9.110.169)";'
+echo "done sending to all servers"
---
+++ ranking-manager/src/main/java/ir/jimbo/rankingmanager/App.java
@@ -0,0 +1,89 @@
+package ir.jimbo.rankingmanager;
+
+import ir.jimbo.rankingmanager.config.ApplicationConfiguration;
+import ir.jimbo.rankingmanager.model.UpdateObject;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.elasticsearch.spark.rdd.api.java.JavaEsSpark;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+
+public class App {
+    private static final Logger LOGGER = LogManager.getLogger(App.class);
+
+    public static void main(String[] args) throws IOException {
+        ApplicationConfiguration appConfig = new ApplicationConfiguration();
+
+        SparkConf sparkConf = new SparkConf();
+        sparkConf.setAppName(appConfig.getAppName());
+        sparkConf.setMaster(appConfig.getMaster());
+        sparkConf.set("es.nodes", appConfig.getElasticSearchNodes());
+        sparkConf.set("es.mapping.id", "id");
+        sparkConf.set("es.index.auto.create", appConfig.getAutoIndexCreate());
+
+        JavaSparkContext javaSparkContext = new JavaSparkContext(sparkConf);
+
+        LOGGER.info("configuring Hbase");
+        Configuration hBaseConfiguration;
+        hBaseConfiguration = HBaseConfiguration.create();
+        hBaseConfiguration.addResource(new Path(System.getenv("HADOOP_CONF_DIR"), "core-site.xml"));
+        hBaseConfiguration.addResource(new Path(System.getenv("HBASE_CONF_DIR"), "hbase-site.xml"));
+        hBaseConfiguration.set(TableInputFormat.INPUT_TABLE, appConfig.getTableName());
+        hBaseConfiguration.set(TableInputFormat.SCAN_COLUMN_FAMILY, appConfig.getColumnFamily());
+
+        JavaRDD<Result> hBaseRDD = javaSparkContext
+                .newAPIHadoopRDD(hBaseConfiguration, TableInputFormat.class
+                        , ImmutableBytesWritable.class, Result.class).values();
+        String columnFamily = appConfig.getColumnFamily();
+
+        JavaRDD<UpdateObject> map = hBaseRDD.map((Function<Result, UpdateObject>) row -> {
+            Map<String, Integer> anchors = new HashMap<>();
+            NavigableMap<byte[], byte[]> familyMap = row.getFamilyMap(Bytes.toBytes(columnFamily));
+            AtomicInteger count = new AtomicInteger();
+            String urlHash;
+            if (row.getRow().length >= 16) {
+                urlHash = new String(Arrays.copyOfRange(row.getRow(), row.getRow().length - 16, row.getRow().length));
+            } else {
+                urlHash = new String(Arrays.copyOfRange(row.getRow(), 0, row.getRow().length));
+            }
+            familyMap.forEach((qualifier, value) -> {
+                count.getAndIncrement();
+                String text = Bytes.toString(value);
+                if (anchors.containsKey(text)) {
+                    anchors.put(text, anchors.get(text) + 1);
+                } else {
+                    anchors.put(text, 1);
+                }
+            });
+            List<String> collect = anchors.entrySet().stream()
+                    .sorted(Map.Entry.comparingByValue()).map(Map.Entry::getKey).collect(Collectors.toList());
+            List<String> topAnchors;
+            if (collect.size() > 10) {
+                topAnchors = Arrays.asList(Arrays.copyOfRange(collect.toArray(new String[0]), collect.size() - 10, collect.size()));
+            } else {
+                topAnchors = collect;
+            }
+            return new UpdateObject(urlHash, count.get(), topAnchors);
+        });
+
+        String indexName = appConfig.getElasticSearchIndexName();
+        LOGGER.info("updating elastic search documents");
+        JavaEsSpark.saveToEs(map, indexName + "/_doc");
+        LOGGER.info("updating elastic search finished");
+    }
+}
