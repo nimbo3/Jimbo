f1e15ccb3c27c07fc0844355f3e091edbde20d6e
smska <mohammadsadeghkeshavarzi@yahoo.com>
2019-08-10 14:13:48 +0430

    Remove ssh.sh from root

--- ssh.sh
+++
@@ -1,8 +0,0 @@
-while read -a host
-do
-	ssh-copy-id -p ${host[1]} ${host[0]}
-	while read -a other_host
-	do
-		echo "ssh-copy-id -p ${other_host[1]} ${other_host[0]}" | ssh -p ${host[1]} ${host[0]}
-	done < $1
-done < $1
