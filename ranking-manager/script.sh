c6cfdef3361c7a6f901c84a51ca5f078fac21d27
smsk <mohammadsadeghkeshavarzi@yahoo.com>
2019-08-13 14:26:03 +0430

    Add scripts for all modules

--- ranking-manager/script.sh
+++ ranking-manager/script.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 mvn clean package -DskipTests
-echo "Sending to the server"
-ssh -p $2 $1 'rm ranking-manager*.jar'
-scp -P $2 target/ranking-manager*.jar $1:~/
+echo "Sending to the first server"
+ssh -p $2 $1 'rm -rf ~/ranking-manager'
+ssh -p $2 $1 'mkdir ~/ranking-manager'
+ssh -p $2 $1 'mkdir ~/ranking-manager/config'
+scp -P $2 target/ranking-manager*.jar $1:~/ranking-manager
+scp -P $2 src/main/resources/* $1:~/ranking-manager/config
+echo "echo 'java -javaagent:\$HOME/prometheus_jmx/jmx_prometheus_javaagent-0.12.0.jar=7777:\$HOME/prometheus_jmx/config.yaml -jar ranking-manager*.jar' > ~/ranking-manager/ranking-manager.sh;" | ssh -p $2 $1
+ssh -p $2 $1 'chmod 775 ~/ranking-manager/ranking-manager.sh'
+echo "Sending to other servers from the first one"
+while read -a host
+do
+    echo "ssh -p ${host[1]} ${host[0]} 'rm -rf ~/ranking-manager'" | ssh -p $2 $1
+    echo "scp -P ${host[1]} -r ~/ranking-manager ${host[0]}:~/" | ssh -p $2 $1
+done < $3
+echo "Done sending to all servers"
+mvn clean
--- search-api/script.sh
+++ search-api/script.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
