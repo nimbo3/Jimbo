c7447fa197495e7c917747b2032aa654df0dcb25
smsk <mohammadsadeghkeshavarzi@yahoo.com>
2019-08-14 20:46:02 +0430

    Unify scripts

--- page-processor/script.sh
+++ page-processor/script.sh
@@ -6,8 +6,8 @@ ssh -p $2 $1 'mkdir ~/page-processor'
 ssh -p $2 $1 'mkdir ~/page-processor/config'
 scp -P $2 target/page-processor*.jar $1:~/page-processor
 scp -P $2 src/main/resources/* $1:~/page-processor/config
-echo "echo 'java -javaagent:\$HOME/prometheus_jmx/jmx_prometheus_javaagent-0.12.0.jar=8765:\$HOME/prometheus_jmx/config.yaml -jar page-processor*.jar' > ~/page-processor/page-processor.sh;" | ssh -p $2 $1
-ssh -p $2 $1 'chmod 775 ~/page-processor/page-processor.sh'
+echo "echo 'java -javaagent:\$HOME/prometheus_jmx/jmx_prometheus_javaagent-0.12.0.jar=8765:\$HOME/prometheus_jmx/config.yaml -jar page-processor*.jar' > ~/page-processor/run.sh;" | ssh -p $2 $1
+ssh -p $2 $1 'chmod 775 ~/page-processor/run.sh'
 echo "Sending to other servers from the first one"
 while read -a host
 do
--- run.sh
+++ run.sh
@@ -3,16 +3,16 @@ EXIT="exit"
 while true; do
     echo "which servers:(seperate with ',') :";
     read SERVERS_STRING;
-    if [ "$SERVERS_STRING" == "$EXIT" ]; then
+    if [[ "$SERVERS_STRING" == "$EXIT" ]]; then
         exit 0;
     fi
     IFS=',' read -ra servers <<< "$SERVERS_STRING";
-    echo "which application(crawler, es-pp or hbase-pp) :";
+    echo "which application(crawler, es-page-processor or page-processor) :";
     read application;
-    if [ "$application" == "$EXIT" ]; then 
+    if [[ "$application" == "$EXIT" ]]; then
         exit 0;
     fi
     for i in "${servers[@]}"; do
-        ssh -p 3031 jimbo@$i './'$application'/run.sh';
+        ssh -p 3031 jimbo@${i} '~/'${application}'/run.sh';
     done
 done
