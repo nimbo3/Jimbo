6b0236d622e3e65d5f36e60cd14996768176bada
seyedalireza <shosseini@ce.sharif.edu>
2019-08-31 01:06:30 +0430

    rankobject is serializable now

--- ranking-manager/src/main/java/ir/jimbo/rankingmanager/model/RankObject.java
+++ ranking-manager/src/main/java/ir/jimbo/rankingmanager/model/RankObject.java
@@ -5,11 +5,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class RankObject {
+public class RankObject implements Serializable {
     private String id;
     private Double rank;
 
